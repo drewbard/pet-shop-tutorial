3fbb562ff212589ed85bebfdc024a1518faa1ec5
Andrew Barrow <abarrow@rokasecurity.com>
2017-11-19 00:01:44 -0800

    Completed tutorial

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pet Shop Truffle Box Tutorial - Completed
+I decided to complete the pet shop [truffle tutorial](http://truffleframework.com/tutorials/pet-shop) in a more es6 way.
+Let me know if you run into any issues.
+
+## Install
+Clone the repo
+```bash
+yarn install
+truffle develop
+truffle(develop)> compile
+truffle(develop)> migrate
+```
+## Run
+```bash
+yarn start
+```
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,165 @@
-App = {
-  web3Provider: null,
-  contracts: {},
+class App {
+	constructor() {
+		this.web3Provider = null;
+		this.contracts = {};
+		this.onAdoptButtonClicked = this.onAdoptButtonClicked.bind(this);
+		
+		this._initAsync()
+			.then(() => console.info('App Loaded Successfully'))
+			.catch(error => console.error(error.message));
+	}
+	
+	async _initAsync() {
+		const pets = await this._loadPetsAsync('../pets.json');
+		this._populateUIWith(pets);
+		this._initWeb3();
+		await this._initAdoptionContractAsync();
+		await this._updateUIWithAdoptedPets();
+		this._bindUIEvents();
+	}
+	
+	_populateUIWith(pets) {
+		const petsList = $('#petsRow');
+		
+		pets.forEach((pet) => {
+			const petComponent = this._buildPetHTMLComponentWith(pet);
+			petsList.append(petComponent);
+		});
+	}
+	
+	_buildPetHTMLComponentWith(pet) {
+		const petTemplate = $('#petTemplate');
+		
+		petTemplate.find('.panel-title').text(pet.name);
+		petTemplate.find('img').attr('src', pet.picture);
+		petTemplate.find('.pet-breed').text(pet.breed);
+		petTemplate.find('.pet-age').text(pet.age);
+		petTemplate.find('.pet-location').text(pet.location);
+		petTemplate.find('.btn-adopt').attr('data-id', pet.id);
+		
+		return petTemplate.html();
+	}
+	
+	_initWeb3() {
+		const web3Provider = this._getWeb3Provider();
+		this.web3Provider = web3Provider;
+		window.web3 = new window.Web3(web3Provider);
+	}
+	
+	_getWeb3Provider() {
+		const web3IsDefined = !!window.web3;
+		
+		if (web3IsDefined) {
+			return window.web3.currentProvider;
+		} else {
+			// Fallback to the TestRPC
+			return new Web3.providers.HttpProvider('http://localhost:8545');
+		}
+	}
+	
+	async _initAdoptionContractAsync() {
+		const adoptionArtifact = await this._loadAdoptionArtifactAsync();
+		
+		// Instantiate adoption contract with truffle-contract
+		this.contracts.Adoption = window.TruffleContract(adoptionArtifact);
+		
+		// Set the provider for our contract
+		this.contracts.Adoption.setProvider(this.web3Provider);
+	}
+	
+	_loadPetsAsync(path) {
+		return new Promise((resolve, reject) => {
+			$.getJSON(path)
+				.done(resolve)
+				.fail(reject);
+		});
+	}
+	
+	_loadAdoptionArtifactAsync() {
+		return new Promise((resolve, reject) => {
+			$.getJSON('Adoption.json')
+				.done(resolve)
+				.fail(reject);
+		});
+	}
+	
+	_bindUIEvents() {
+		$(document).on('click', '.btn-adopt', this.onAdoptButtonClicked);
+	}
+	
+	async _getOwnersAsync() {
+		try {
+			const deployedAdoptionInstance = await this.contracts.Adoption.deployed();
+			return deployedAdoptionInstance.getAdopters.call();
+		} catch (error) {
+			throw error;
+		}
+	}
+	
+	_markPetPanelToAdopted(petIndex) {
+		const petPanels = document.getElementsByClassName('panel-pet');
+		
+		const petPanelButton = petPanels[petIndex].getElementsByTagName('button')[0];
+		const shouldUpdate = petPanelButton.innerHTML !== 'Success';
+		
+		if(shouldUpdate) {
+			petPanelButton.innerText = 'Success';
+			petPanelButton.disabled = true;
+		}
+	}
+	
+	async _updateUIWithAdoptedPets() {
+		const owners = await this._getOwnersAsync();
+		
+		owners.forEach((address, ownerIndex) => {
+			
+			const hasOwner = address !== '0x0000000000000000000000000000000000000000';
+			
+			if (hasOwner) {
+				this._markPetPanelToAdopted(ownerIndex);
+			}
+		});
+	}
+	
+	_getAccountsAsync() {
+		return new Promise((resolve, reject) => {
+			web3.eth.getAccounts((error, accounts) => {
+				if (error) {
+					reject(error)
+				}
+				
+				resolve(accounts);
+			});
+		});
+	}
+	
+	async _adoptPetAsync(petId) {
+		const accounts = await this._getAccountsAsync();
+		const fromAccount = accounts[0];
+		const adoptionInstance = await this.contracts.Adoption.deployed();
+		
+		// Execute adopt as a transaction by sending account
+		return adoptionInstance.adopt(petId, {from: fromAccount});
+	}
+	
+	async _adoptPet(petId) {
+		await this._adoptPetAsync(petId);
+		await this._updateUIWithAdoptedPets();
+	}
+	
+	onAdoptButtonClicked(event) {
+		event.preventDefault();
+		
+		const petId = parseInt($(event.target).data('id'));
+		
+		this._adoptPet(petId)
+			.then(() => console.info('Successfully adopted'))
+			.catch(console.error);
+	}
+}
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
-};
-
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+$(function () {
+	$(window).load(function () {
+		window.app = new App();
+	});
 });
